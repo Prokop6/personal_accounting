08ce7204d879dc2dfcf779cb37635443d776773e
Marcin <mr.prokop.6+dev@gmail.com>
2023-11-29 00:09:02 +0000

    partners table, transaction view, additional grants

--- init.sql
+++ init.sql
@@ -5,16 +5,44 @@ CREATE DATABASE accounting with owner root;
 
 USE accounting; 
 
+CREATE TABLE partners (
+  id bigserial primary key,
+  short_name text not null,
+  long_name text,
+  address text,
+  misc text, 
+  alias_of_id bigserial
+);
+
 CREATE TABLE transactions (
 id bigserial primary key,
 date date not null,
-partner text not null,
+partner_id bigserial,
 account text not null,
 payment_method text not null,
 currency text not null,
-sum NUMERIC(10,2) not null
+sum NUMERIC(10,2) not null,
+CONSTRAINT fk_partner_id
+  FOREIGN KEY(partner_id)
+  REFERENCES partners(id)
 );
 
-GRANT all privileges on accounting.public.transactions to root;
+CREATE OR REPLACE VIEW transactions_view (
+  Date, 
+  Partner,
+  Account,
+  payment_method, 
+  currency,
+  sum
+) AS
+  select date, partners.short_name, account, payment_method, currency, sum
+  from transactions 
+  join
+  partners on transactions.partner_id = partners.id
+  ORDER BY date
+  ;
+
+GRANT INSERT, SELECT on accounting.public.transactions to root;
+GRANT INSERT, SELECT on accounting.public.partners to root;
 GRANT USAGE, select on ALL SEQUENCES IN SCHEMA public to root;
 
